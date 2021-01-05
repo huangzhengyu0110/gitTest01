d555d5458d86f865933db42fa309f66087f537ff
hzy <wy5030620@163.com>
2021-01-05 19:12:56 +0800

    新增方法注解，同时方法名改变

--- test/src/cn/edu/fudan/test01.java
+++ test/src/cn/edu/fudan/test01.java
@@ -8,7 +8,8 @@ public class test01 {
         }
     }
 
-    public void method02() {
+    @Deprecated
+    public void method04() {
         System.out.println("outerMethod1");
     }
 
@@ -22,7 +23,7 @@ public class test01 {
         test01 object = new test01();
         test01Inner obj = object.new test01Inner();
         obj.method01();
-        object.method02();
+        object.method04();
         object.method03();
 
     }
